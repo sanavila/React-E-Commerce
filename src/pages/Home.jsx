044a5ab457a43f5e487f028c5642a86abbd8e6ad
sanavila <rute.leone@hotmail.com>
2023-01-17 11:52:45 -0300

    display-flex and badge icon in Navbar component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
-      
+      <Navbar />
     </div>
   )
 }
+
+export default Home
